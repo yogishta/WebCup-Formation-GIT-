9eda8a34e3a2c8aac4ef501053c8cc1a8f75690b
yogishta <yteeroovengadum@astek.mu>
2017-08-28 14:13:42 +0400

    Added Webcup Folder

---
+++ WEBCUP BACKUP/nashscript.js
@@ -0,0 +1,35 @@
+
+	$(document).ready(function(){
+    $(".fishClown").click(function(){
+        $("#myFishClown").modal('show');
+    });
+	
+	$(".seaTurtle").click(function(){
+        $("#mySeaTurtle").modal('show');
+    });
+	
+	$(".octopus").click(function(){
+        $("#myOctopus").modal('show');
+    });
+	
+	$(".dolphin").click(function(){
+        $("#myDolphin").modal('show');
+    });
+	
+	$(".kelp").click(function(){
+        $("#myKelp").modal('show');
+    });
+	
+	$(".posidonia").click(function(){
+        $("#myPosidonia").modal('show');
+    });
+	
+	$(".milfoils").click(function(){
+        $("#myMilfoils").modal('show');
+    });
+	
+	$(".waterwheel").click(function(){
+        $("#myWaterwheel").modal('show');
+    });
+	
+});
